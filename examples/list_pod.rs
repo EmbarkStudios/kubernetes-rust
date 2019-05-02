4f4868b1628a57b763cf7589845298ff51088103
Jake Shadle <jake.shadle@embark-studios.com>
2019-05-02 16:34:52 +0200

    Update examples and tests

--- examples/list_pod.rs
+++ examples/list_pod.rs
@@ -1,18 +1,15 @@
-extern crate failure;
-extern crate k8s_openapi;
-extern crate kubernetes;
+#![feature(await_macro, async_await)]
 
 use k8s_openapi::api::core::v1 as api;
 use kubernetes::client::APIClient;
 use kubernetes::config;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let kubeconfig = config::load_kube_config().expect("failed to load kubeconfig");
     let kubeclient = APIClient::new(kubeconfig);
     let (req, _) = api::Pod::list_namespaced_pod("kube-system", Default::default())
         .expect("failed to create a request");
-    let list_pod = kubeclient
-        .request::<api::PodList>(req)
-        .expect("failed to list up pods");
+    let list_pod = await!(kubeclient.request::<api::PodList>(req)).expect("failed to list up pods");
     println!("{:?}", list_pod);
 }
